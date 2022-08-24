d03760cc641d76915861ff6fd8bcd66d62ac4def
CrisYnz <crisyanezb@gmail.com>
2022-08-23 21:51:19 -0400

    fin clase 1 semana 2

--- semana2/f20220823/js/script.js
+++ semana2/f20220823/js/script.js
@@ -1,21 +1,72 @@
-//alert("Hola mundo!");
+// alert("Hola Mundo!!");
 
-/**
- * Este es un comentario mult-linea
- */
+/* 
+este es un comentario
+multilínea
+*/
 
 window.addEventListener('DOMContentLoaded', (e) => {
-    // con el evento DOMContentLoaded me aseguro que todas las etiquetas HTML fueron cargadas y procesadas
-
+    // con el evento DOMContentLoaded me aseguró que todas las etiquetas HTML 
+    // fueron cargadas y procesadas por el browser 
+    // más info en: https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
     console.log("evento DOMContentLoaded");
+
+    // Sintaxis de variables:
+    // let nombreVariable = valor;
+    // más ejemplos
+    //let nombre = "Santiago";
+    //let edad   = 34; 
 
     let boton = document.getElementById("btn-suscribir");
     boton.addEventListener("click", (ev) => {
-        let nombre = document.getElementById("nombre").value;
-        console.log(`El nombre del suscriptor es ${nombre}`)
-    })
+        // recuperar los valores del formulario 
+        let nombre  = document.getElementById("nombre").value;
+        let email   = document.getElementById("correo").value;
+        let genero  = getGenero();
+        let intereses =getIntereses(); 
+        let suscriptor = { // JSON =  JavaScript object notation 
+            // clave: valor
+            nombre_completo: nombre,  //nombre,
+            email, // email: email,
+            genero,
+            intereses,
+            fecha_registro: (new Date()).toISOString() 
+        };        
+        console.dir(suscriptor);
+        guardarSuscriptor(suscriptor);
+    });    
+});
 
+function guardarSuscriptor (suscriptor){
+    const url = "https://curso-frontend-67796-default-rtdb.firebaseio.com/suscriptores.json"
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(suscriptor)
+    });
+}
+function getIntereses(){
+    let inputIntereses = document.querySelectorAll("input[name='intereses']:checked");
+    let arrIntereses = [];
+    inputIntereses.forEach( nodoInteres => arrIntereses.push(nodoInteres.value));
+    if (inputIntereses.length < 1){
+        mostrarError("Debe seleccionar al menos un tema de interés!!")
+        return false
+    }
+    return arrIntereses
+}
 
-})
+function getGenero() {
+    let inputSeleccionado = document.querySelector("input[name='genero']:checked")
+    if ( inputSeleccionado == null ) {
+        mostrarError("Debe seleccionar un género!!!");
+        return false 
+    }
+    const genero = inputSeleccionado.value;
+    return genero;
+}
 
-console.log("Hola mundo!");
+function mostrarError(mensajeDeError) {
+    console.error(mensajeDeError); 
+}
+
+console.log("Hola Mundo!!");
